0dca83acd08de537ce81330f17be367f59dedddd
ElenaShab <Lenuska999@yandex.ru>
2021-10-14 19:18:56 +0300

    Add head and button

--- ABCalculator.py
+++ ABCalculator.py
@@ -2,10 +2,26 @@
 
 import tkinter as tk
 
+#Закрытие программы
+def do_close():
+    root.destroy()
+
 # Главное окно
 root = tk.Tk()
 root.geometry("280x300")
 root.title("A/B калькулятор")
+
+#Метка заголовка
+lblTitle = tk.Label(text = "A/B калькулятор", font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
+lblTitle.place(x=55, y=20)
+
+#Кнопка "Расчитать"
+btnProcess = tk.Button(root, text="Рассчитать", font = ('Helvetica', 10, 'bold'), fg = '#0000cc')
+btnProcess.place(x=25, y=250, width=90, height=30)
+
+#Кнопка закрытия программы
+btnClose = tk.Button(root, text="Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close)
+btnClose.place(x=160, y=250, width=90, height=30)
 
 # mainloop
 root.mainloop()
